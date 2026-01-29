f5cc1bcf1ee9d9f2adf30ea116a09590dd801853
Beila <beilah@nycstudents.net>
2026-01-29 14:02:46 -0500

    s

--- app/src/app.js
+++ app/src/app.js
@@ -1,1 +1,13 @@
 import './styles.css';
+
+let answers= [
+    "Yes.",
+    "No.",
+    "Maybe.",
+    "The universe doesn't answer...",
+    "Don't count on it.",
+    "Ask again later.",
+    "As I see it, yes.",
+    "For now, no.",
+    "Is the sky blue?",
+]
