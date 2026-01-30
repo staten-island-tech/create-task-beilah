1d95cbd093e9aa76f2db0b238c47593786fbf038
Beila <beilah@nycstudents.net>
2026-01-30 14:01:39 -0500

    asdasd

--- app/src/app.js
+++ app/src/app.js
@@ -11,3 +11,28 @@ let answers= [
     "For now, no.",
     "Is the sky blue?",
 ]
+
+const form = document.querySelector(".askButton");
+const questionInput = document.querySelector("#askInput");
+
+form.addEventListener("submit", (a) => {
+    a.preventDefault();
+
+    const question = questionInput.value;
+
+    const randomIndex = Math.floor(Math.random() * answers.length);
+    const randomAnswer = answers[randomIndex];
+
+    console.log("Question:", question);
+    console.log("Answer:", randomAnswer);
+
+    const ansDiv = document.querySelector(".ansDiv");
+    
+    ansDiv.insertAdjacentHTML(
+        "beforeend",
+        `<h3 class="question text-center">Question: ${question}</h3>
+        <h3 class="answer text-center">Answer: ${randomAnswer}</h3>`
+    );
+
+    form.reset();
+})
